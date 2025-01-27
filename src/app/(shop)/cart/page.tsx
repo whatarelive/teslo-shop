077bec757fd2f7a92f92c0637edd91f7d101c547
adrian daniel gutierrez martinez <adriand@estudiantes.uci.cu>
2025-01-27 03:02:24 +0100

    Maquetación intermedia de la página de carrito de compra

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -26,32 +26,63 @@ export default function CartPage() {
             <Link href="/" className="underline mb-5">
               Continúa comprando
             </Link>
+
+            {/* Items */}
+            {
+              productsInCart.map((product) => (
+                <div key={ product.slug } className="flex mt-5">
+                  <Image
+                    src={`/products/${product.images[0]}`}
+                    width={ 100 }
+                    height={ 100 }
+                    alt={ product.title }
+                    className="mr-5 w-[100px] h-[100px] rounded"
+                  />
+
+                  <div>
+                    <p>{ product.title }</p>
+                    <p>${ product.price }</p>
+                    <SelectedQuantity quantity={ 3 }/>
+
+                    <button className="underline mt-3">
+                      Remover
+                    </button>
+                  </div>
+                </div>
+              ))
+            }
           </div>
 
-          {/* Items */}
-          {
-            productsInCart.map((product) => (
-              <div key={ product.slug } className="flex">
-                <Image
-                  src={`/products/${product.images[0]}`}
-                  width={ 100 }
-                  height={ 100 }
-                  alt={ product.title }
-                  className="mr-5 rounded"
-                />
+          {/* Checkout */}
+          <div className="bg-white rounded-xl shadow-xl p-7">
+            <h2 className="text-2xl mb-2">
+              Resumen de orden
+            </h2>
 
-                <div>
-                  <p>{ product.title }</p>
-                  <p>{ product.price }</p>
-                  <SelectedQuantity quantity={ 3 }/>
+            <div className="grid grid-cols-2">
+              <span>No. Productos</span>
+              <span className="text-right">$ 100</span>
 
-                  <button className="underline mt-3">
-                    Remover
-                  </button>
-                </div>
-              </div>
-            ))
-          }
+              <span>Subtotal</span>
+              <span className="text-right">$ 100</span>
+
+              <span>Impuestos (15%)</span>
+              <span className="text-right">$ 100</span>
+
+              <span className="mt-5 text-2xl">Total</span>
+              <span className="mt-5 text-2xl text-right">$ 100</span>
+            </div>
+
+            <div className="mt-5 mb-2 w-full">
+              <Link 
+                href="/checkout/address" 
+                className="flex btn-primary justify-center"
+              >
+                Checkout
+              </Link>
+            </div>
+          </div>
+
         </div>
       </div>
     </div>
